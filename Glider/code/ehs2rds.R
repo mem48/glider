1e2a4cebfd1197c8ce82404240fb92f10fcbb8c0
Malcolm Morgan <earmmor@leeds.ac.uk>
2017-03-03 18:14:56 +0000

    end of week wrap up

--- Glider/code/ehs2rds.R
+++ Glider/code/ehs2rds.R
@@ -1,6 +1,6 @@
 #Read in EHS and convert to RDS
 library(foreign)
-library(dplyr)
+library(dplyr, lib.loc = "M:/R/R-3.3.1/library")
 
 infld <- "C:/Users/earmmor/OneDrive/OD/Glider - Private/WP2/Data/EHS/EHS-2013-SPSS/UKDA-7802-spss/spss/spss19/"
 
@@ -25,10 +25,10 @@ elevate <- elevate[,c( "aacode","Felsolff","Felpvff","Felcavff","Felextff",
 
 #Services Table
 services <- read.spss(paste0(infld,"physical/services.sav"),to.data.frame=TRUE)
-services <- services[,c("aacode","Finchloc","Finchtyp","Findisty","Finmhfue","Finmhboi","Finchbag",
+services <- services[,c("aacode","Finchtyp","Findisty","Finmhfue","Finmhboi","Finchbag",
                         "Finchoff","Finchthe","Finchtim","Finchove","Finchrom","Finchcon","Finchtrv","Finchtzc","Finchdst",
                         "Finoheat","Finohphs","Finohtyp",
-                        "Finwheat","Finwhcpr","Finwhopr","Finwhoty","Finwhoac","Finwhoag","Finwhxpr","Finwhxty","Finwhxag",
+                        "Finwhoty","Finwhoac","Finwhoag","Finwhxpr","Finwhxty","Finwhxag",
                         "Finwsipr","Finwsiag","Finwdipr","Finwdiag",
                         "Finwsppr","Finwspty","Finwspag",
                         "Finwmppr","Finwmpty","Finwmpag",
@@ -123,19 +123,169 @@ context <- combined[c("aacode","tenure4x","vacantx","GorEHCS","rumorph","rucombi
 
 
 #Energy Table
-energy <- combined[c("aacode","Finchloc","Finchtyp","Findisty","mainfuel","Finmhboi","Finchbag",
+energy <- combined[c("aacode","Finchtyp","Findisty","mainfuel","Finmhboi","Finchbag","heatsec","sysage","watersys",
                      "Finchoff","Finchthe","Finchtim","Finchove","Finchrom","Finchcon","Finchtrv","Finchtzc","Finchdst",
-                     "Finoheat","Finohphs","Finohtyp",
-                     "Finwheat","Finwhcpr","Finwhopr","Finwhoty","Finwhoac","Finwhoag","Finwhxpr","Finwhxty","Finwhxag",
+                     "Finohphs",
+                     "Finwhoty","Finwhoag","Finwhxpr","Finwhxty","Finwhxag",
                      "Finwsipr","Finwsiag","Finwdipr","Finwdiag",
-                     "Finwsppr","Finwspty","Finwspag",
-                     "Finwmppr","Finwmpty","Finwmpag",
+                     "Finwsppr",
+                     "Finwmppr",
                      "Finwhlpr","Finwhlty","Finwhlag",
                      "Finwhcyl","Finwhsiz","Finwhins","Finwhmms",
-                     "Finwhcen","Finwhthe",
-                     "heat7x","heatsec","sysage","watersys"
+                     "Finwhcen","Finwhthe"
+                     
                      )]
 
+#Remove Nulls from Controls
+
+energy$Finchoff <- rem_na(energy$Finchoff) #On/Off
+energy$Finchthe <- rem_na(energy$Finchthe) #Boiler Thermostat
+energy$Finchove <- rem_na(energy$Finchove) #Manual Overide
+energy$Finchrom <- rem_na(energy$Finchrom) #Room Thermostat
+energy$Finchcon <- rem_na(energy$Finchcon) #Radiator Control
+energy$Finchtrv <- rem_na(energy$Finchtrv) #Thermostatic Radiator Conrol
+energy$Finchtzc <- rem_na(energy$Finchtzc) #Time and Temp Zone
+energy$Finchdst <- rem_na(energy$Finchdst) #Delayed Start
+
+energy$control <- NA
+
+# Energy Control Levle cumulative number of controls had
+#L6 - Delay Start, L5 - Time and temp zones, L4 - Room Thermostat, L3 - Boiler Thermostat, L2 - Manual Overide, L1 - On/Off, L0 - None, L9 - Other combination
+
+for(l in 1:nrow(energy)){
+  if(energy$Finchdst[l] == "Yes" && energy$Finchtzc[l] == "Yes" && energy$Finchrom[l] == "Yes" && energy$Finchthe[l] == "Yes" && energy$Finchove[l] == "Yes" && energy$Finchoff[l] == "Yes"){
+    energy$control[l] <- 6
+  } else if (energy$Finchtzc[l] == "Yes" && energy$Finchrom[l] == "Yes" && energy$Finchthe[l] == "Yes" && energy$Finchove[l] == "Yes" && energy$Finchoff[l] == "Yes"){
+    energy$control[l] <- 5
+  } else if (energy$Finchrom[l] == "Yes" && energy$Finchthe[l] == "Yes" && energy$Finchove[l] == "Yes" && energy$Finchoff[l] == "Yes"){
+    energy$control[l] <- 4
+  } else if (energy$Finchthe[l] == "Yes" && energy$Finchove[l] == "Yes" && energy$Finchoff[l] == "Yes"){
+    energy$control[l] <- 3
+  } else if (energy$Finchove[l] == "Yes" && energy$Finchoff[l] == "Yes"){
+    energy$control[l] <- 2
+  } else if (energy$Finchoff[l] == "Yes"){
+    energy$control[l] <- 1
+  } else if (energy$Finchdst[l] == "No" && energy$Finchtzc[l] == "No" && energy$Finchrom[l] == "No" && energy$Finchthe[l] == "No" && energy$Finchove[l] == "No" && energy$Finchoff[l] == "No"){
+    energy$control[l] <- 0
+  } else {
+    energy$control[l] <- 9
+  }
+}
+
+hist(energy$control)
+
+#Radiator Control Level
+#L2 - TRVs, L1 - Controls, L0 - None
+
+energy$radcontrol <- NA
+for(m in 1:nrow(energy)){
+  if(energy$Finchtrv[m] == "Yes"){
+    energy$radcontrol[m] <- 2
+  } else if (energy$Finchtrv[m] == "No" && energy$Finchcon[m] == "Yes"){
+    energy$radcontrol[m] <- 1
+  } else {
+    energy$radcontrol[m] <- 0
+  }
+}
+hist(energy$radcontrol)
+
+#Simplify Water Cylinder and Imersion Heater
+
+energy$Finwsipr <- rem_na(energy$Finwsipr) #Single Immersion
+energy$Finwdipr <- rem_na(energy$Finwdipr) #Dual Immersion
+energy$Finwhcyl <- rem_na(energy$Finwhcyl) #Water Cyclinder
+
+energy$tank <-NA
+energy$immersage <- NA
+for(n in 1:nrow(energy)){
+  if((energy$Finwsipr[n] == "Yes" || energy$Finwdipr[n] == "Yes") && energy$Finwhcyl[n] == "Yes"){
+    energy$tank[n] <- "Tank with Immersion Heater"
+    if(is.na(energy$Finwsiag[n])){
+      energy$immersage[n] <- energy$Finwdiag[n]
+    }else{
+      energy$immersage[n] <- energy$Finwsiag[n]
+    }
+  }else if((energy$Finwsipr[n] == "No" && energy$Finwdipr[n] == "No") && energy$Finwhcyl[n] == "Yes"){
+    energy$tank[n] <- "Tank without Immersion Heater"
+  }else{
+    energy$tank[n] <- "No Tank"
+  }
+}
+
+#Simplify Instaneous Heater
+energy$Finwmppr <- rem_na(energy$Finwmppr) #Mulitpoint
+energy$Finwsppr <- rem_na(energy$Finwsppr) #Single Point
+
+energy$instant <- NA
+for(o in 1:nrow(energy)){
+  if(energy$Finwmppr[o] == "Yes" && energy$Finwsppr[o] == "Yes"){
+    energy$instant[o] <- "Yes"
+  }else if(energy$Finwmppr[o] == "No" && energy$Finwsppr[o] == "Yes"){
+    energy$instant[o] <- "Yes"
+  }else if(energy$Finwmppr[o] == "Yes" && energy$Finwsppr[o] == "No"){
+    energy$instant[o] <- "Yes"
+  }else{
+    energy$instant[o] <- "No"
+  }
+}
+
+levels(energy$Finchtyp) <- c(levels(energy$Finchtyp),"No Answer")
+#Clean Main ones
+for(q in 1:nrow(energy)){
+  if(is.na(energy$Finmhboi[q])){
+    energy$Finmhboi[q] <- "No boiler"
+  }
+  if(is.na(energy$Finchtyp[q])){
+    energy$Finchtyp[q] <- "No Answer"
+  }
+  
+}
+
+
+
+
+
+test <- energy[,c("Finchtyp","Finmhboi","watersys","instant","tank")]
+uni <- unique(test)
+#Count the uniqes
+uni$count <- NA
+for(p in 1:nrow(uni)){
+  uni$count[p] <- nrow(test[test$Finchtyp == uni$Finchtyp[p] & test$Finmhboi == uni$Finmhboi[p] & test$watersys == uni$watersys[p] & test$instant == uni$instant[p] & test$tank == uni$tank[p] ,])
+}
+
+
+
+write.csv(uni,"uni.csv")
+
+
+#Simplify Water System
+#for(p in 1:nrow(energy)){
+#  if(energy$Finwhcpr[p] == "Yes" & energy$Finwhopr[p] == "No")
+#}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+energy <- energy[c("aacode","Finchtyp","Findisty","mainfuel","Finmhboi","Finchbag","heatsec","sysage","watersys",
+                   "Finohphs",
+                   "Finwhcpr","Finwhopr","Finwhoty","Finwhoag","Finwhxpr","Finwhxty","Finwhxag",
+                   "Finwhlpr","Finwhlty","Finwhlag",
+                   "Finwhsiz","Finwhins","Finwhmms",
+                   "Finwhcen","Finwhthe","control", "radcontrol","immersage","instant","tank"
+                   
+)]
 
 
 #Mulit-row per dwelling tables
