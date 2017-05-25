d4f71676b85572bbc7987d641ca4c42eff50d58e
Malcolm Morgan <earmmor@leeds.ac.uk>
2017-05-25 11:48:17 +0100

    architype groups

--- Glider/code/similar_archetypes.R
+++ Glider/code/similar_archetypes.R
@@ -17,8 +17,8 @@ ncell <-  nrow ** 2
 y_last = 1
 
 for(i in 1:ncell){
-  y <- floor(i/nrow - 0.00000000000001) + 1 #Column Number
-  x <- i - ((y-1) * nrow) #Row Number
+  x <- if(i %% nrow == 0){nrow}else{i %% nrow}
+  y <- ((i-1) %/% nrow) + 1
   if(y != y_last){print(paste0("Doing column ",y," of ",nrow," at ",Sys.time()))}
   yid <- colnames(comp)[y]
   xid <- rownames(comp)[x]
@@ -28,5 +28,70 @@ for(i in 1:ncell){
   y_last <- y
 }
 
+#comp$V1858 <- NULL #Don't know why this happend
+saveRDS(comp,"data/archetypes_comparion2.Rds")
+rm(nrow,ncell,x,y,yid,xid,xval,yval,y_last, uni, sel, i)
+#stop()
 
-saveRDS(comp,"data/archetypes_comparion.Rds")
+#Sort by count then group with simialr architypes
+rowcount <- data.frame(id = names(comp), sum=rowSums(comp)) #This one for editing
+groups <- data.frame(id = names(comp), sum=rowSums(comp) ,group=NA, ndwel=NA) # this one to store
+rowcount <- rowcount[order(-rowcount$sum),] # Sort
+groups$id <- as.character(groups$id)
+
+#Add in the numebr of dwellings
+for(b in 1:nrow(groups)){
+  groups$ndwel[b] <- sum(comb$aagpd1213[comb$archcode == groups$id[b]]) 
+}
+rm(b)
+
+#Group the Architypes
+for(a in 1:99999){
+  id <- as.character(rowcount$id[1])
+  sub <- as.data.frame(comp[,id, drop = FALSE])
+  names(sub) <- c("res")
+  sub <- sub[sub$res >=6,,drop = FALSE]
+  getids <- rownames(sub)
+  groups$group[groups$id %in% getids] <- a
+  rowcount <- rowcount[!(rowcount$id %in% getids),]
+  if(nrow(rowcount) == 0){
+    break
+  }
+}
+rm(a, id,sub, getids)
+
+
+
+
+#Count the number of dwellings in each group
+groupsums <- data.frame(group = groups$group[!duplicated(groups$group)], ndwel = NA)
+for(c in 1:nrow(groupsums)){
+  groupsums$ndwel[c] <- sum(groups$ndwel[groups$group == groupsums$group[c]])
+}
+rm(c)
+
+groupsums <- groupsums[order(-groupsums$ndwel),]
+
+csum <- cumsum(groupsums$ndwel)
+plot(csum, ylab = "Number of Houses", xlab = "Number of Archetype Groups")
+
+#Add in architype codes
+groupsums$arch <- NA
+for(d in 1:nrow(groupsums)){
+  groupsums$arch[d] <- groups$id[groups$group == groupsums$group[d]][1]
+}
+
+
+#uni$count <- NA
+#uni$countsamp <- NA
+#uni$conf <- NA
+#for (f in 1:nrow(uni)){
+#  uni$count[f] <- sum(sel$aagpd1213[sel$archcode == uni$archcode[f] ])
+#  uni$countsamp[f] <- nrow(sel[sel$archcode == uni$archcode[f]  , ])
+#  uni$conf[f] <- uni$count[f] / uni$countsamp[f]
+#}
+
+
+
+
+
