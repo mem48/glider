00e9aec977a0c95e2fae80862d1c2da7aa403f25
Malcolm Morgan <earmmor@leeds.ac.uk>
2017-06-12 17:33:15 +0100

    Archityp work

--- Glider/code/similar_archetypes.R
+++ Glider/code/similar_archetypes.R
@@ -1,56 +1,126 @@
+library(parallel)
+
 # To find siliar artchitypes
 comb <- readRDS("data/combined_2013_arch.Rds")
 sel <- comb[,c("aacode","aagpd1213","archcode","energytyp","generaltyp","rooftyp","solartyp","walltyp","wintyp","floortyp")]
+rm(comb)
 
 sel <- sel[with(sel, order(rooftyp,solartyp,walltyp,wintyp,floortyp)),]
-uni <- as.data.frame(unique(sel[,c("archcode","energytyp","generaltyp","rooftyp","solartyp","walltyp","wintyp","floortyp")]))
+sel$generaltyp2 <- substr(sel$generaltyp,1,1)
+#uni <- as.data.frame(unique(sel[,c("energytyp","rooftyp","solartyp","walltyp","wintyp","floortyp")]))
+#uni$subcode <- paste0(uni$rooftyp,uni$solartyp,uni$walltyp,uni$wintyp,uni$floortyp)
+#sel$subcode <- paste0(sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp,sel$energytyp)
+sel$subcode <- paste0(sel$generaltyp2,sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp)
 
 
-comp <- matrix(nrow = nrow(uni), ncol = nrow(uni))
-comp <- as.data.frame(comp)
-names(comp) <- uni$archcode
-rownames(comp) <- uni$archcode
+#test <- sel[sel$generaltyp == "I",]
+test <- sel
+grp <- data.frame(id = unique(test$subcode), nhouse = NA)
+grp$id <- as.character(grp$id)
 
-nrow <- length(uni$archcode)
-ncell <-  nrow ** 2
-
-y_last = 1
-
-for(i in 1:ncell){
-  x <- if(i %% nrow == 0){nrow}else{i %% nrow}
-  y <- ((i-1) %/% nrow) + 1
-  if(y != y_last){print(paste0("Doing column ",y," of ",nrow," at ",Sys.time()))}
-  yid <- colnames(comp)[y]
-  xid <- rownames(comp)[x]
-  xval <- uni[uni$archcode == xid,]
-  yval <- uni[uni$archcode == yid,]
-  comp[x,y] <- sum(xval$energytyp == yval$energytyp, xval$generaltyp == yval$generaltyp, xval$rooftyp == yval$rooftyp, xval$solartyp == yval$solartyp, xval$walltyp == yval$walltyp, xval$wintyp == yval$wintyp, xval$floortyp == yval$floortyp)
-  y_last <- y
+for(b in 1:nrow(grp)){
+  grp$nhouse[b] <- sum(test$aagpd1213[test$subcode == grp$id[b]]) 
 }
 
-#comp$V1858 <- NULL #Don't know why this happend
-saveRDS(comp,"data/archetypes_comparion2.Rds")
-rm(nrow,ncell,x,y,yid,xid,xval,yval,y_last, uni, sel, i)
-#stop()
+grp <- grp[order(-grp$nhouse),]
+csum <- cumsum(grp$nhouse)
+check <- csum < sum(grp$nhouse) * 0.8
+plot(csum)
+grp_sub <- grp[check,]
+csum2 <- cumsum(grp_sub$nhouse)
+points(csum2, col = "Red")
+grp_sub$strip <- substr(grp_sub$id,2,6)
+
+grp2 <- data.frame(id = unique(grp_sub$strip), nhouse = NA)
+for(b in 1:nrow(grp2)){
+  grp2$nhouse[b] <- sum(grp_sub$nhouse[grp_sub$strip == grp2$id[b]]) 
+} 
+
+#compare <- data.frame(constr = grp2$id, semi = NA, detached = NA, mid = NA, end = NA, bungalow = NA, converted = NA, low = NA, high = NA)
+sel$strip <- paste0(sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp)
+
+
+
+#comp <- matrix(nrow = nrow(uni), ncol = nrow(uni))
+#comp <- as.data.frame(comp)
+#names(comp) <- uni$archcode
+#rownames(comp) <- uni$archcode
+
+row <- grp2$id
+row <- as.character(row)
+col <- unique(sel$generaltyp2)
+
+
+nrow <- length(grp2$id)
+ncell <-  nrow * 8
+
+#Do as lapply
+countarch <- function(i){
+  x <- if(i %% 8 == 0){8}else{i %% 8}
+  y <- ((i-1) %/% nrow) + 1
+  yid <- row[y]
+  xid <- col[x]
+  res <- sum(sel$aagpd1213[sel$generaltyp2 == xid & sel$strip == yid])
+  return(res)
+}
+
+
+compare <- lapply(1:ncell,countarch)
+compare <- matrix(unlist(compare), ncol = 8, byrow = FALSE)
+compare <- as.data.frame(compare)
+names(compare) <- col
+rownames(compare) <- row
+
+##### Something gone wrong as totals add up to more than the number of housing in england
+
+
+
+
+
+
+
+
+#Do as lapply
+countsimilar <- function(i){
+  x <- if(i %% nrow == 0){nrow}else{i %% nrow}
+  y <- ((i-1) %/% nrow) + 1
+  yid <- uni$subcode[y]
+  xid <- uni$subcode[x]
+  xval <- uni[uni$subcode == xid,]
+  yval <- uni[uni$subcode == yid,]
+  res <- sum(xval$energytyp == yval$energytyp, xval$rooftyp == yval$rooftyp, xval$solartyp == yval$solartyp, xval$walltyp == yval$walltyp, xval$wintyp == yval$wintyp, xval$floortyp == yval$floortyp)
+  return(res)
+}
+
+compare <- lapply(1:ncell,countsimilar)
+compare <- matrix(unlist(compare), ncol = nrow, byrow = FALSE)
+compare <- as.data.frame(compare)
+names(compare) <- uni$subcode
+rownames(compare) <- uni$subcode
+
+saveRDS(compare,"data/archetypes_comparion3.Rds")
+rm(nrow,ncell,x,y,yid,xid,xval,yval, uni, i)
+
 
 #Sort by count then group with simialr architypes
-rowcount <- data.frame(id = names(comp), sum=rowSums(comp)) #This one for editing
-groups <- data.frame(id = names(comp), sum=rowSums(comp) ,group=NA, ndwel=NA) # this one to store
+rowcount <- data.frame(id = names(compare), sum=rowSums(compare)) #This one for editing
+groups <- data.frame(id = names(compare), sum=rowSums(compare) ,group=NA, ndwel=NA) # this one to store
 rowcount <- rowcount[order(-rowcount$sum),] # Sort
 groups$id <- as.character(groups$id)
 
 #Add in the numebr of dwellings
+
 for(b in 1:nrow(groups)){
-  groups$ndwel[b] <- sum(comb$aagpd1213[comb$archcode == groups$id[b]]) 
+  groups$ndwel[b] <- sum(sel$aagpd1213[sel$subcode == groups$id[b]]) 
 }
-rm(b)
+
 
 #Group the Architypes
 for(a in 1:99999){
   id <- as.character(rowcount$id[1])
-  sub <- as.data.frame(comp[,id, drop = FALSE])
+  sub <- as.data.frame(compare[,id, drop = FALSE])
   names(sub) <- c("res")
-  sub <- sub[sub$res >=6,,drop = FALSE]
+  sub <- sub[sub$res >=5,,drop = FALSE]
   getids <- rownames(sub)
   groups$group[groups$id %in% getids] <- a
   rowcount <- rowcount[!(rowcount$id %in% getids),]
@@ -59,8 +129,6 @@ for(a in 1:99999){
   }
 }
 rm(a, id,sub, getids)
-
-
 
 
 #Count the number of dwellings in each group
