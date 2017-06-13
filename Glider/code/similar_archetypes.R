a58478db3c70f11a957b4c1088a7cd92cea051bb
Malcolm Morgan <earmmor@leeds.ac.uk>
2017-06-13 17:57:02 +0100

    Fixed Archetypes

--- Glider/code/similar_archetypes.R
+++ Glider/code/similar_archetypes.R
@@ -1,4 +1,4 @@
-library(parallel)
+#library(parallel)
 
 # To find siliar artchitypes
 comb <- readRDS("data/combined_2013_arch.Rds")
@@ -7,60 +7,62 @@ rm(comb)
 
 sel <- sel[with(sel, order(rooftyp,solartyp,walltyp,wintyp,floortyp)),]
 sel$generaltyp2 <- substr(sel$generaltyp,1,1)
-#uni <- as.data.frame(unique(sel[,c("energytyp","rooftyp","solartyp","walltyp","wintyp","floortyp")]))
-#uni$subcode <- paste0(uni$rooftyp,uni$solartyp,uni$walltyp,uni$wintyp,uni$floortyp)
-#sel$subcode <- paste0(sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp,sel$energytyp)
-sel$subcode <- paste0(sel$generaltyp2,sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp)
+sel$subcode <- paste0(sel$generaltyp2,sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp, sel$energytyp)
 
+sum(sel$aagpd1213) #23253781
 
-#test <- sel[sel$generaltyp == "I",]
-test <- sel
-grp <- data.frame(id = unique(test$subcode), nhouse = NA)
+grp <- data.frame(id = unique(sel$subcode), nhouse = NA)
 grp$id <- as.character(grp$id)
 
 for(b in 1:nrow(grp)){
-  grp$nhouse[b] <- sum(test$aagpd1213[test$subcode == grp$id[b]]) 
+  grp$nhouse[b] <- sum(sel$aagpd1213[sel$subcode == grp$id[b]]) 
 }
+
+sum(sel$aagpd1213) #23253781
+
 
 grp <- grp[order(-grp$nhouse),]
 csum <- cumsum(grp$nhouse)
 check <- csum < sum(grp$nhouse) * 0.8
 plot(csum)
+
+sum(sel$aagpd1213[sel$subcode %in% grp$id]) #23253781
+sum(grp$nhouse)#23253781
+
 grp_sub <- grp[check,]
 csum2 <- cumsum(grp_sub$nhouse)
 points(csum2, col = "Red")
-grp_sub$strip <- substr(grp_sub$id,2,6)
+
+sum(sel$aagpd1213[sel$subcode %in% grp_sub$id]) #18541922
+
+grp_sub$strip <- substr(grp_sub$id,2,7)
 
 grp2 <- data.frame(id = unique(grp_sub$strip), nhouse = NA)
 for(b in 1:nrow(grp2)){
   grp2$nhouse[b] <- sum(grp_sub$nhouse[grp_sub$strip == grp2$id[b]]) 
 } 
 
-#compare <- data.frame(constr = grp2$id, semi = NA, detached = NA, mid = NA, end = NA, bungalow = NA, converted = NA, low = NA, high = NA)
-sel$strip <- paste0(sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp)
+sum(grp2$nhouse) #18541922
+
+sel$strip <- paste0(sel$walltyp,sel$floortyp,sel$rooftyp,sel$solartyp,sel$wintyp,sel$energytyp)
 
 
-
-#comp <- matrix(nrow = nrow(uni), ncol = nrow(uni))
-#comp <- as.data.frame(comp)
-#names(comp) <- uni$archcode
-#rownames(comp) <- uni$archcode
-
-row <- grp2$id
+row <- grp2$id #To do without energy
 row <- as.character(row)
 col <- unique(sel$generaltyp2)
 
 
-nrow <- length(grp2$id)
+nrow <- length(row)
 ncell <-  nrow * 8
 
 #Do as lapply
 countarch <- function(i){
-  x <- if(i %% 8 == 0){8}else{i %% 8}
+  x <- if(i %% nrow == 0){nrow}else{i %% nrow}
   y <- ((i-1) %/% nrow) + 1
-  yid <- row[y]
-  xid <- col[x]
-  res <- sum(sel$aagpd1213[sel$generaltyp2 == xid & sel$strip == yid])
+  yid <- col[y]
+  xid <- row[x]
+  res <- sum(sel$aagpd1213[sel$generaltyp2 == yid & sel$strip == xid]) # With out energy
+  #res <- sum(sel$aagpd1213[sel$generaltyp2 == yid & sel$subcode == xid]) # With energy
   return(res)
 }
 
@@ -70,11 +72,15 @@ compare <- matrix(unlist(compare), ncol = 8, byrow = FALSE)
 compare <- as.data.frame(compare)
 names(compare) <- col
 rownames(compare) <- row
+sum(compare) 
 
-##### Something gone wrong as totals add up to more than the number of housing in england
+compare$total <- rowSums(compare)
 
 
 
+stop()
+
+###########################################################################
 
 
 
@@ -100,6 +106,8 @@ rownames(compare) <- uni$subcode
 
 saveRDS(compare,"data/archetypes_comparion3.Rds")
 rm(nrow,ncell,x,y,yid,xid,xval,yval, uni, i)
+
+
 
 
 #Sort by count then group with simialr architypes
