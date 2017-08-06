630a4ecf23947760e8c83d91d9c49719dfa10450
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2017-08-06 11:01:14 +0100

    updadted common accounts

--- Glider/twitter/remove_common.R
+++ Glider/twitter/remove_common.R
@@ -1,10 +1,35 @@
 #Remove common twitter accounts
 
+
+
 #read in list of connections
 
-connect.all <- readRDS(paste0("twitter/data/Connections-livedump-2017-08-04.Rds"))
+connect.all <- readRDS(paste0("D:/Users/Malcolm/OneDrive - University of Leeds/Glider - Private/WP3/twitterdata/data/Connections-livedump-2017-08-04.Rds"))
 
 large <- connect.all[connect.all$followersCount > 100000,]
 large <- large[!duplicated(large$screenName),]
 
 write.csv(large,"twitter/data/commonaccounts.csv")
+
+
+#read in to 1000
+celeb <- read.csv("twitter/data/Top-1000-Celebrity-Twitter-Accounts.csv")
+celeb <- celeb[!duplicated(celeb$twitter),]
+
+topfol <- read.csv("twitter/data/commonaccounts.csv", stringsAsFactors = F)
+core <- read.csv("twitter/accounts_cores.csv", stringsAsFactors = F)
+
+keep <- core$Name
+keep <- c(keep, topfol$screenName[topfol$type == "KEEP"])
+keep <- unique(keep)
+
+remove <- topfol$screenName[topfol$type != "KEEP"]
+remove <- c(remove,celeb$twitter)
+remove <- unique(remove)
+remove <- remove[!(remove %in% keep)]
+
+nrow(connect.all)
+connect.all <- connect.all[!(connect.all$screenName %in% remove),]
+nrow(connect.all)
+
+
