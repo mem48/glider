69d0fdc653359a0f92f9d13bbad506f14dff836e
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2017-08-29 15:40:19 +0100

    updated summary sheets

--- Glider/code/summary_sheets.R
+++ Glider/code/summary_sheets.R
@@ -5,19 +5,23 @@ library(knitr)
 library(rmarkdown)
 
 ## Data
-arch <- read.csv("data/archetype_summary.csv", stringsAsFactors = FALSE)
+#arch <- read.csv("data/archetype_summary.csv", stringsAsFactors = FALSE)
+arch <- readRDS("data/archetype_summary_retrofitopts.Rds")
 arch <- arch[order(-arch$ndwel),]
+
+retros <- read.csv("data/retrofit_options.csv", stringsAsFactors = F)
 
 #Trim to top 80%
 
 plot(cumsum(arch$ndwel))
 arch <- arch[cumsum(arch$ndwel) < 0.8 * sum(arch$ndwel),]
-points(cumsum(arch.trim$ndwel), col = "Red")
+points(cumsum(arch$ndwel), col = "Red")
 
-
+m <- 1
+n <- 3 #nrow(arch)
 
 ## Loop
-for (i in 1:nrow(arch)){
+for (i in m:n){
   rmarkdown::render(input = "code/summary_sheet_template.Rmd",
                     output_format = "word_document",
                     output_file = paste("archetype-",arch$archcode[i], ".doc", sep=''),
